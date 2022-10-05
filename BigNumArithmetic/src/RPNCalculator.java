6689adcfe6efd340f70e57705332d0614f534ea8
rm1738 <89798814+rm1738@users.noreply.github.com>
2022-10-05 19:02:21 -0400

    Update RPNCalculator.java

--- BigNumArithmetic/src/RPNCalculator.java
+++ BigNumArithmetic/src/RPNCalculator.java
@@ -1,4 +1,22 @@
- public static LinkedList<Integer> add(
+
+@SuppressWarnings("rawtypes")
+public class RPNCalculator extends LinkedList {
+
+    // Takes a linkedlist as a parameter and returns a reversed linkedlist
+    public static LinkedList<Integer> reverseLinkedList(
+        LinkedList<Integer> n1) {
+        LinkedList<Integer> revLinkedList = new LinkedList<Integer>();
+        for (int i = n1.size() - 1; i >= 0; i--) {
+
+            // Append the elements in reverse order
+            revLinkedList.insert(n1.getObjectAtIndex(i));
+        }
+        // Return the reversed arraylist
+        return revLinkedList;
+    }
+
+
+    public static LinkedList<Integer> add(
         LinkedList<Integer> num1,
         LinkedList<Integer> num2) {
 
@@ -19,19 +37,26 @@
         Integer carry = 0;
         boolean carryN = false;
         int i = 0;
+        
+       
 
         while (i < lSize) {
 
             n1 = num1.getObjectAtIndex(i);
             n2 = num2.getObjectAtIndex(i);
-
+            
             if (n1 == null) {
 
                 r = n2 + carry;
                 result.insert(r % 10);
                 carry = 0;
+                
+                if(r>=10)
+                {
+                    carryN = true; 
+                }
 
-                if (r > 9) {
+                if (carryN = true) {
                     carry = (r % 100) / 10;
 
                     if (i == lSize - 1) {
@@ -45,7 +70,7 @@
                 result.insert(r % 10);
                 carry = 0;
 
-                if (r > 9) {
+                if (r >= 10) {
                     carry = (r % 100) / 10;
                     if (i == lSize - 1) {
                         result.insert(carry);
@@ -60,7 +85,7 @@
                 result.insert(r % 10);
                 carry = 0;
 
-                if (r > 9) {
+                if (r >= 10) {
 
                     carry = (r % 100) / 10;
 
@@ -76,3 +101,40 @@
 
     }
 
+
+    public static LinkedList<Integer> multi(
+        LinkedList<Integer> n1,
+        LinkedList<Integer> n2) {
+
+        LinkedList<Integer> result = new LinkedList<Integer>();
+        int num1 = 0;
+        int num2 = 0;
+        int res = 0;
+        LinkedList<Integer>.Node<Integer> first = n1.head();
+        LinkedList<Integer>.Node<Integer> second = n2.head();
+
+        while (first.next() != null || second.next() != null) {
+
+            if (n1 != null) {
+                num1 = num1 * 10 + first.getData();
+                first = first.next();
+
+            }
+
+            if (n2 != null) {
+
+                num2 = num2 * 10 + second.getData();
+                second = second.next();
+
+            }
+
+            res = num1 * num2;
+            result.insert(res % 10);
+            res = res / 10;
+
+        }
+        return result;
+
+    }
+
+}
