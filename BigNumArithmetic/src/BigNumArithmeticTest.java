2ea41b21523e59bd06d3c45c8b9d8fed8215a9cc
rm1738 <89798814+rm1738@users.noreply.github.com>
2022-10-13 12:21:00 -0400

    Create BigNumArithmeticTest.java

---
+++ BigNumArithmetic/src/BigNumArithmeticTest.java
@@ -0,0 +1,18 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import student.TestCase;
+
+public class BigNumArithmeticTest extends TestCase {
+
+    public void test() throws FileNotFoundException {
+
+        BigNumArithmetic.main(new String[] { "SampleInput.txt" });
+       
+            System.out.println(new String[] { "SampleInput.txt" });
+        
+
+        
+    }
+
+}
